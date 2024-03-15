3f68f8a66f19cbe98ea2cd650998836debf8c82b
Muhammad Osama <thegame9555@gmail.com>
2024-03-15 21:59:26 +0200

    path fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
 import unittest
+import sys
+import os
+
+# Add the directory containing 'app.py' to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
 class TestFlaskApp(unittest.TestCase):
